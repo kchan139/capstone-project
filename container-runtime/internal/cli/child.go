f8974afc43c87337f814cf92d9b8c2bad32cfcfd
Phiung23 <phiung23122004@gmail.com>
2025-08-20 20:48:53 +0700

    feat: add isolation to the overlayfs. Each will not affect others

--- container-runtime/internal/cli/child.go
+++ container-runtime/internal/cli/child.go
@@ -6,24 +6,34 @@ import (
     "os/exec"
     "syscall"
     "my-capstone-project/internal/runtime"
+    "my-capstone-project/internal/utils"
+
 )
 
 func childCommand() error {
     fmt.Printf("Running %v as %d\n", os.Args[2:], os.Getpid())
+    container_id,errstr := utils.RandomHexString(16)
+    if errstr != nil {
+        return fmt.Errorf("failed to generate random hex strings for container ID: %v", errstr)
+    }
     
     // Set hostname
-    if err := syscall.Sethostname([]byte("container")); err != nil {
+    if err := syscall.Sethostname([]byte(container_id)); err != nil {
         return fmt.Errorf("failed to set hostname: %v", err)
     }
     
     // Setup overlay filesystem
     //TODO: need to get rid of hardcode image
-    if err := runtime.SetupOverlayFS("/home/phiung/container_image/fake_ubuntu","/tmp/container-overlay/upper","/tmp/container-overlay/work"); err != nil {
+    fmt.Printf("This is the container ID of this containter:%v\n",container_id)
+    if err := runtime.SetupOverlayFS(container_id,"/home/phiung/container_image/fake_ubuntu"); err != nil {
         return fmt.Errorf("failed to setup overlay: %v", err)
     }
+    merge_path:= fmt.Sprintf("/tmp/container-overlay/%s/merged",container_id)
+    merge_putold_path:= fmt.Sprintf("/tmp/container-overlay/%s/merged/put_old",container_id)
+    os.MkdirAll(merge_putold_path, 0755) 
     
     // Pivot root
-    if err := runtime.PivotRoot("/tmp/container-overlay/merged", "/tmp/container-overlay/merged/put_old"); err != nil {
+    if err := runtime.PivotRoot(merge_path, merge_putold_path); err != nil {
         return fmt.Errorf("failed to pivot root: %v", err)
     }
     
---
+++ container-runtime/internal/utils/randomhexstring.go
@@ -0,0 +1,14 @@
+package utils
+import (
+	"crypto/rand"
+	"encoding/hex"
+)
+
+func RandomHexString(length int) (string, error) {
+	bytes := make([]byte, length)
+	_, err := rand.Read(bytes)
+	if err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(bytes), nil
+}
